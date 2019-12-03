d3768c5c0ba41bc0a416bc9117f63ae611656dbb
Pontus Palmenäs <pontus@palmenas.se>
2019-12-03 23:00:32 +0100

    day3 wip

---
+++ day3/day3.py
@@ -0,0 +1,19 @@
+moves1 = []
+moves2 = []
+wire1 = []
+wire2 = []
+
+def manhattan_dist():
+
+
+def build_wire(moves):
+	for move in moves:
+		print(f'{move[0]}:{move[1:]}')
+
+def build_part(move):
+	if (move[0]
+
+with open('example1') as f:
+	moves1 = f.readline().rstrip('\n').split(",")
+	moves2 = f.readline().rstrip('\n').split(",")
+
