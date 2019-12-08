2afec5d4ca43f33ec7860387f6ad182f91ec7cce
Pontus Palmenäs <pontus@palmenas.se>
2019-12-08 20:37:54 +0100

    Added comment to explain hack

--- day4/day4b.py
+++ day4/day4b.py
@@ -1,5 +1,4 @@
 def test(p):
-	double_digits = False
 	doubles = {"0":0,"1":0,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0}
 
 	prev_c = p[0]
@@ -13,6 +12,7 @@ def test(p):
 		
 		prev_c = c
 
+	# Find all characters which fit only two adjacent doubles (e.g 111122 => 2)
 	fits = {k: v for k, v in doubles.items() if v == 1}
 	return sum(map(len, fits)) > 0
 
