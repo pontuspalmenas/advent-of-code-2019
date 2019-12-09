a070bd6ada5ccacf533899fed55cf08e660a49b7
Pontus Palmenäs <pontus@palmenas.se>
2019-12-09 22:06:11 +0100

    wip

--- day8/day8b-test.py
+++ day8/day8b-test.py
@@ -16,25 +16,26 @@ for c in line:
         n = 0
 
 top_layer = ['x' for i in range(w*h)]
-prev_layer = None
 
 # get the highest previous layers value that is non-transparent
-def get_highest(ix, curr_layer):
-    for i in range(0, curr_layer):
-        if layers[i][ix] != '2':
-            return layers[i][ix]
+def get_highest(pos_in_layer, curr_layer):
+    for i in range(curr_layer, len(layers)-1):
+        if layers[i][pos_in_layer] != '2':
+            return layers[i][pos_in_layer]
 
-
-print('test')
-print(get_highest(3, 4))
-print(get_highest(0, 1))
-
-for i, l in enumerate(layers[::-1]):
+for curr_layer, l in enumerate(layers):
+    print(l)
     for n in range(0, w*h):
         if l[n] == '2':
-            top_layer[n] = get_highest(n, i)
+            print('?')
+            top_layer[n] = get_highest(n, curr_layer)
         else:
-            top_layer[n] = l[n]
-    prev_layer = l            
-
+            if top_layer[n] != '0' and top_layer[n] != '1':
+                top_layer[n] = l[n]
+    print(top_layer)
 print(top_layer)
+
+
+
+# 0222112222120000
+# layers = [0222, 1122, 2212, 0000]
